30d41233be4e2e44ce5f9347c97d3d677018f54f
cristian <cpadureac@gmail.com>
2019-03-30 20:46:31 +0200

    First commit

---
+++ FileManager/CloudFileManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Cloud.Storage.V1;
+
+
+namespace FileManager
+{
+    class CloudFileManager : IFileManager
+    {
+
+        StorageClient _storageClient;
+        public CloudFileManager(string jsonPath)
+        {
+            var credential = GoogleCredential.FromFile(jsonPath);
+            _storageClient = StorageClient.Create(credential);
+        }
+        public void CheckExists(string filePath, string fileName)
+        {
+
+            //var name = 'file_i_want_to_check.txt'; 
+            //var storage_client = storage.Client();
+            //var = 'my_bucket_name';
+            
+            //var bucketName = _storageClient.GetBucket(filePath);
+            //_storageClient.Ex
+            //var stats = storage.Blob(bucket = bucketName, name = fileName).exists(storage_client);
+
+        }
+    public void DeleteFile(string filePath, string fileName)
+    {
+
+    }
+    public void ListFilesFromPath(string path)
+    {
+
+    }
+    public void GetFileContent(string filePath, string fileName)
+    {
+
+    }
+    public void StoreFile(string inputData, string outputPath, string outputFileName)
+    {
+
+    }
+}
+}
---
+++ FileManager/IFileManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileManager
+{
+    interface IFileManager
+    {
+        void CheckExists(string filePath, string fileName);
+        void DeleteFile(string filePath, string fileName);
+        void ListFilesFromPath(string path);
+        void GetFileContent(string filePath, string fileName);
+        void StoreFile(string inputData, string outputPath, string outputFileName);
+    }
+}
