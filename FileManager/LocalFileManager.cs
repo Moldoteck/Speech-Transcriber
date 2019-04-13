34f5aef3df1826ae8d318432199fb6cc2a78bf21
Octanny <octanny90@yahoo.com>
2019-04-13 16:01:55 +0300

    Update return types

---
+++ FileManager/LocalFileManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileManager
+{
+    class LocalFileManager : IFileManager
+    {
+        public void CheckExists(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteFile(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ListFilesFromPath(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StoreFile(string inputData, string outputPath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
