c21b244641ab5e5743d381ba2875c463b2b8437d
Octanny <octanny90@yahoo.com>
2019-04-13 15:50:26 +0300

    Update IFileManager interface

--- FileManager/IFileManager.cs
+++ FileManager/IFileManager.cs
@@ -6,10 +6,9 @@ namespace FileManager
 {
     interface IFileManager
     {
-        void CheckExists(string filePath, string fileName);
-        void DeleteFile(string filePath, string fileName);
-        void ListFilesFromPath(string path);
-        void GetFileContent(string filePath, string fileName);
-        void StoreFile(string inputData, string outputPath, string outputFileName);
+        void CheckExists(string filePath);
+        void DeleteFile(string filePath);
+        void ListFilesFromPath(string filePath);
+        void StoreFile(string inputData, string outputPath);
     }
 }
