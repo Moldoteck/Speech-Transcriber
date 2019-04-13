e05c2a3a23f4fa8980ae6ad072b603e5f00bf7c1
Octanny <octanny90@yahoo.com>
2019-04-13 17:05:56 +0300

    Add AudioFile

---
+++ AudioFile/AudioFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AudioFile
+{
+    public class AudioFile : IAudioFile
+    {
+        private string _filePath;
+
+        public int GetChannel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetFrameRate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
