f417c47338ca53bd318ba0c960ee8827a582d896
Octanny <octanny90@yahoo.com>
2019-04-13 16:47:57 +0300

    Update AudioFileConverter

---
+++ AudioFileConverter/AudioFileConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using NAudio.Wave;
+
+namespace AudioFileConverter
+{
+    public class AudioFileConverter : IAudioFileConverter
+    {
+        public int ConvertToFormat(string filePath, string outputFilePath)
+        {
+            string inputFormat = Path.GetExtension(filePath); // .mp3 sau .wav sau ...
+            string outputFormat = Path.GetExtension(outputFilePath);
+            if (inputFormat != ".mp3" || outputFormat != ".wav")
+                return -1;
+            using (Mp3FileReader mp3 = new Mp3FileReader(filePath))
+            {
+                using (WaveStream pcm = WaveFormatConversionStream.CreatePcmStream(mp3))
+                {
+                    WaveFileWriter.CreateWaveFile(outputFilePath, pcm);
+                }
+            }
+            return 1;
+        }
+
+        public int StereoToMono(string filePath, string outputFilePath)
+        {
+            string inputFormat = Path.GetExtension(filePath); // .mp3 sau .wav sau ...
+            string outputFormat = Path.GetExtension(outputFilePath);
+            if (inputFormat != ".wav" || outputFormat != ".wav")
+                return -1;
+            using (var waveFileReader = new WaveFileReader(filePath))
+            {
+                var toMono = new StereoToMonoProvider16(waveFileReader);
+                WaveFileWriter.CreateWaveFile(outputFilePath, toMono);
+            }
+            return 1;
+        }
+    }
+}
--- AudioFileConverter/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AudioFileConverter
-{
-    public class Class1
-    {
-    }
-}
